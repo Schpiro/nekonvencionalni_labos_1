859fc19aac4b1b155878caccd3ef689230e21210
Borna Bilandzija <borna.bilandzija@outlook.com>
2022-08-18 23:52:24 +0200

    Added better logging and fixed logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
-    public static final Integer popSize = 10;
-    public static final Integer boardSize = 3;
+    public static final Integer popSize = 10000;
+    public static final Integer boardSize = 60;
+    public static final Integer chanceToMutate = 50;
     public static final Random random = new Random();
 
     public static List<BoardState> genPop(int popSize, int boardSize)
@@ -18,11 +19,11 @@ public class Main {
         return bs;
     }
 
-    public static List<BoardState> recombination(Integer nRekomb, BoardState bs1, BoardState bs2)
+    public static List<BoardState> recombination(BoardState bs1, BoardState bs2)
     {
         List<Integer> r1 = new ArrayList<>();
         List<Integer> r2 = new ArrayList<>();
-        nRekomb = random.nextInt(boardSize);
+        Integer nRekomb = random.nextInt(boardSize);
         for(int i = 0; i < nRekomb; i++)
         {
             r1.add(bs1.getArray().get(i));
@@ -38,13 +39,13 @@ public class Main {
         BoardState bs1_fin = new BoardState(r1);
         BoardState bs2_fin = new BoardState(r2);
         int mut = random.nextInt(100);
-        if(mut <= 5)
+        if(mut <= chanceToMutate)
         {
             int mutGen = random.nextInt(boardSize);
             bs1_fin.getArray().set(mutGen, random.nextInt(boardSize));
         }
         mut = random.nextInt(100);
-        if(mut <= 5)
+        if(mut <= chanceToMutate)
         {
             int mutGen = random.nextInt(boardSize);
             bs2_fin.getArray().set(mutGen, random.nextInt(boardSize));
@@ -55,60 +56,42 @@ public class Main {
         return comb;
     }
 
+    public static BoardState tournament(List<BoardState> boardStates){
+        List<BoardState> bsCpy = new ArrayList<>(boardStates);
+
+        int rand1 = random.nextInt(popSize);
+
+        BoardState r1 = bsCpy.get(rand1);
+        bsCpy.remove(rand1);
+        int rand2 = random.nextInt(popSize-1);
+        BoardState r2 = bsCpy.get(rand2);
+        bsCpy.remove(rand2);
+        int rand3 = random.nextInt(popSize-2);
+        BoardState r3 = bsCpy.get(rand3);
+        bsCpy.remove(rand3);
+
+        List<BoardState> kandidati = new ArrayList<>();
+        kandidati.add(r1);
+        kandidati.add(r2);
+        kandidati.add(r3);
+        kandidati.sort((_r1, _r2) -> _r1.fitness().compareTo(_r2.fitness()));
+        return kandidati.get(0);
+    }
+
     public static List<BoardState> kTurnir(List<BoardState> boardStates)
     {
         List<BoardState> newGen = new ArrayList<>();
-        List<BoardState> medPop = new ArrayList<>();
-        for(int i = 0; i < popSize; i++)
+
+        for(int i = 0; i < popSize/2; i++)
         {
             List<BoardState> bsCpy = new ArrayList<>(boardStates);
-
-            int rand1 = random.nextInt(popSize);
-            BoardState r1 = bsCpy.get(rand1);
-            bsCpy.remove(rand1);
-            int rand2 = random.nextInt(popSize-1);
-            BoardState r2 = bsCpy.get(rand2);
-            bsCpy.remove(rand2);
-            int rand3 = random.nextInt(popSize-2);
-            BoardState r3 = bsCpy.get(rand3);
-            bsCpy.remove(rand3);
-
-
-            /*int rand1 = random.nextInt(boardSize);
-            BoardState r1 = bsCpy.get(rand1);
-            bsCpy.remove(rand1);
-            int rand2 = random.nextInt(boardSize);
-            BoardState r2 = bsCpy.get(rand2);
-            bsCpy.remove(rand2);
-            int rand3 = random.nextInt(boardSize);
-            BoardState r3 = bsCpy.get(rand3);
-            bsCpy.remove(rand3);*/
-
-            List<BoardState> kandidati = new ArrayList<>();
-            kandidati.add(r1);
-            kandidati.add(r2);
-            kandidati.add(r3);
-            kandidati.sort((_r1, _r2) -> _r1.fitness().compareTo(_r2.fitness()));
-            medPop.add(kandidati.get(0));
-            //System.out.println("kandidati: \n"+kandidati);
-        }
-        for(int i = 0; i <(popSize/2)-3; i++)
-        {
-            List<BoardState> bsCpy = new ArrayList<>(medPop);
-
-            int rand1 = random.nextInt(popSize);
-            BoardState r1 = bsCpy.get(rand1); //random.nextInt(max - min) + min
-            bsCpy.remove(rand1);
+            BoardState r1 = tournament(bsCpy);
             BoardState r2;
-            int rand2;
-            do {
-                rand2 = random.nextInt(popSize - 1);
-                r2 = bsCpy.get(rand2);
-            }while(r1.equals(r2));
+            do{
+                r2 = tournament(bsCpy);
+            }while ((r1.getArray()).equals(r2.getArray()));
 
-            bsCpy.remove(rand2);
-
-            List<BoardState> child = new ArrayList<>(recombination(4, r1, r2));
+            List<BoardState> child = new ArrayList<>(recombination(r1, r2));
             //System.out.println("Parents: \n"+r1 +" " +r2 +"Childs: \n" + child);
             newGen.add(child.get(0));
             newGen.add(child.get(1));
@@ -117,17 +100,19 @@ public class Main {
         }
         //System.out.println("MEDPOP:\n "+ medPop);
         boardStates.sort((bs1, bs2) -> bs1.fitness().compareTo(bs2.fitness()));
-        System.out.println(boardStates);
-
-        return boardStates;
+        //System.out.println(boardStates);
+        boardStates.subList(popSize,popSize*2).clear();
+        //System.out.println(boardStates);
+        return newGen;
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         List<BoardState> boardStates = new ArrayList<>();
         boardStates = genPop(popSize, boardSize);
 
         boardStates.sort((bs1, bs2) -> bs1.fitness().compareTo(bs2.fitness()));
-        System.out.print(boardStates + " ");
+        //System.out.print(boardStates + " ");
         for(int i = 0; i < boardStates.size(); i++)
         {
             System.out.print(boardStates.get(i).fitness() + " ");
@@ -142,18 +127,20 @@ public class Main {
             boardStates.sort((_bs1, _bs2) -> _bs1.fitness().compareTo(_bs2.fitness()));
             count++;
             System.out.println("new gen no: " + count);
+            //System.out.println(boardStates);
             if(count == 4000)
             {
                 break;
             }
             System.out.print(boardStates.get(0).fitness() + ",");
-            System.out.println(boardStates);
             if(boardStates.get(0).fitness().equals(0))
                 break;
         }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
 
-        System.out.print("NEW GEN\n" + boardStates);
+        System.out.print("FINAL GEN\n" + boardStates);
+        System.out.println("FINAL GEN NUMBER:" + count);
+        System.out.println("TIME TO EXECUTE: " + duration + "ms");
     }
-
-
 }
